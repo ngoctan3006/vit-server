7f74e316a74f70898ff0cca7d49d9c916b3173d6
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-01-12 18:50:31 +0700

    add constant

---
+++ server/middleware/constants.js
@@ -0,0 +1,2 @@
+export const CAN_VIEW_GROUP = ['admin', 'boss', 'group_chief', 'group_vice'];
+export const ADMIN = 'admin';
--- server/middleware/group.js
+++ server/middleware/group.js
@@ -1,17 +1,18 @@
+import { CAN_VIEW_GROUP } from './constants.js';
+
 export const authorizeViewGroup = (req, res, next) => {
-	console.log(req);
-	if (canViewGroup(req.positions, ['admin', 'boss', 'group_chief', 'group_vice']))
-		next();
-	else {
-		return res.status(403).json({
-			success: false,
-			message: 'Not authorized'
-		});
-	}
+    console.log(req);
+    if (canViewGroup(req.positions, CAN_VIEW_GROUP)) next();
+    else {
+        return res.status(403).json({
+            success: false,
+            message: 'Not authorized'
+        });
+    }
 };
 
 function canViewGroup(positions, authorized) {
-    return positions.some(element => {
+    return positions.some((element) => {
         return authorized.includes(element);
-    })
+    });
 }
