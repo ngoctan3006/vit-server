6ddc47537e7018a0c1c831d6825ff3553f43b4a8
namln-hust <nam.ln183956@sis.hust.edu.vn>
2022-01-14 16:27:49 +0700

    add activities apis and events apis

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,14 @@
 import users from './users.js';
 import groups from './groups.js';
+import events from './events.js';
+import activities from './activities.js';
 import vit from './vit.js';
 
 export default (app) => {
     app.use('/api/v1/users', users);
     app.use('/api/v1/groups', groups);
+    app.use('/api/v1/events', events);
+    app.use('/api/v1/activities', activities);
     app.use('/api/v1/vit', vit);
 
     app.use('/', (req, res) => {
