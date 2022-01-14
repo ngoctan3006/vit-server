86aea271305aaccf1c119d4fb0ed58f2283cbd10
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-01-15 00:16:07 +0700

    add club api

--- server/routes/clubs.js
+++ server/routes/clubs.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Club from '../models/Club.js';
+import verifyToken from '../middleware/auth.js';
 
 const router = express.Router();
 
@@ -14,6 +15,31 @@ router.get('/', async (req, res) => {
         res.status(500).json({
             success: false,
             error
+        });
+    }
+});
+
+router.post('/', verifyToken, async (req, res) => {
+    const { name, department, description, chief, vice, members, reference } =
+        req.body;
+
+    const newClub = new Club({
+        name,
+        department,
+        description,
+        chief,
+        vice,
+        members,
+        reference
+    });
+
+    try {
+        await newClub.save();
+        res.status(201).send({ success: true, data: newClub });
+    } catch (err) {
+        res.status(400).send({
+            success: false,
+            err
         });
     }
 });
