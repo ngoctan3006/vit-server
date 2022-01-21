807429f1bb919c1adf7fe69020992767cb58be39
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-01-21 14:08:53 +0700

    add department route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,18 @@
-import users from './users.js';
-import groups from './groups.js';
-import clubs from './clubs.js';
-import events from './events.js';
 import activities from './activities.js';
+import clubs from './clubs.js';
+import departments from './departments.js';
+import events from './events.js';
+import groups from './groups.js';
+import users from './users.js';
 import VIT from './vit.js';
 
 export default (app) => {
-    app.use('/api/v1/users', users);
-    app.use('/api/v1/groups', groups);
-    app.use('/api/v1/clubs', clubs);
-    app.use('/api/v1/events', events);
     app.use('/api/v1/activities', activities);
+    app.use('/api/v1/clubs', clubs);
+    app.use('/api/v1/departments', departments);
+    app.use('/api/v1/events', events);
+    app.use('/api/v1/groups', groups);
+    app.use('/api/v1/users', users);
     app.use('/api/v1/vit', VIT);
 
     app.use('/', (req, res) => {
