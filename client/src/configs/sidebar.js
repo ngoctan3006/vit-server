76d7cd00df38d4786ad6b878a84f711dfa93333f
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-01-25 22:15:42 +0700

    config sidebar

---
+++ client/src/configs/sidebar.js
@@ -0,0 +1,39 @@
+const sidebarItems = [
+    {
+        name: 'Trang chủ',
+        link: '/',
+        icon: 'bx bx-home-alt'
+    },
+    {
+        name: 'Mảng',
+        link: '/departments',
+        icon: 'bx bx-home-alt'
+    },
+    {
+        name: 'Câu lạc bộ',
+        link: '/clubs',
+        icon: 'bx bx-home-alt'
+    },
+    {
+        name: 'Nhóm',
+        link: '/groups',
+        icon: 'bx bx-group'
+    },
+    {
+        name: 'Hoạt động',
+        link: '/activities',
+        icon: 'bx bx-home-alt'
+    },
+    {
+        name: 'Sự kiện',
+        link: '/events',
+        icon: 'bx bx-home-alt'
+    },
+    {
+        name: 'Thành viên',
+        link: '/members',
+        icon: 'bx bx-home-alt'
+    }
+];
+
+export default sidebarItems;
