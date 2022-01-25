4f79545e278e87e1212292381e3fb901efb36106
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-01-25 22:15:11 +0700

    add sidebar

---
+++ client/src/components/sidebar/Sidebar.js
@@ -0,0 +1,42 @@
+import { Link, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import sidebarItems from '../../configs/sidebar';
+import logo from '../../assets/images/logo.png';
+import './sidebar.css';
+
+const Sidebar = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const location = useLocation();
+
+    useEffect(() => {
+        const currPath = location.pathname;
+        const activeItem = sidebarItems.findIndex(
+            (item) => item.link === currPath
+        );
+
+        setActiveIndex(activeItem);
+    }, [location]);
+
+    return (
+        <div className='sidebar'>
+            <div className='sidebar__logo'>
+                <img src={logo} alt='logo' />
+            </div>
+            {sidebarItems.map((item, index) => (
+                <Link to={item.link} key={index}>
+                    <div className='sidebar__item'>
+                        <div
+                            className={`sidebar__item-inner ${
+                                activeIndex === index && 'active'
+                            }`}>
+                            <i className={item.icon}></i>
+                            <span>{item.name}</span>
+                        </div>
+                    </div>
+                </Link>
+            ))}
+        </div>
+    );
+};
+
+export default Sidebar;
