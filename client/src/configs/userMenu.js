14b4fbe93bfd707dfcaa6a1974fb1614af30725b
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-01-26 02:08:19 +0700

    add fake data

---
+++ client/src/configs/userMenu.js
@@ -0,0 +1,16 @@
+const userMenu = [
+    {
+        icon: 'bx bx-user',
+        content: 'Profile'
+    },
+    {
+        icon: 'bx bx-cog',
+        content: 'Settings'
+    },
+    {
+        icon: 'bx bx-log-out-circle bx-rotate-180',
+        content: 'Logout'
+    }
+];
+
+export default userMenu;
