730ef840c4e2d7f07f41a01f9886ce800c58196f
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-01-25 22:16:11 +0700

    setup front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './assets/css/main.css';
+import Layout from './components/layout/Layout';
+import Activities from './pages/Activities';
+import Clubs from './pages/Clubs';
+import Dashboard from './pages/Dashboard';
+import Departments from './pages/Departments';
+import Events from './pages/Events';
+import Groups from './pages/Groups';
+import Members from './pages/Members';
 
-function App() {
+const App = () => {
     return (
-        <div className='App'>
-            <header className='App-header'>
-                <img src={logo} className='App-logo' alt='logo' />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className='App-link'
-                    href='https://reactjs.org'
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Layout />}>
+                    <Route index element={<Dashboard />} />
+                    <Route path='/departments' element={<Departments />} />
+                    <Route path='/clubs' element={<Clubs />} />
+                    <Route path='/groups' element={<Groups />} />
+                    <Route path='/activities' element={<Activities />} />
+                    <Route path='/events' element={<Events />} />
+                    <Route path='/members' element={<Members />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
-}
+};
 
 export default App;
