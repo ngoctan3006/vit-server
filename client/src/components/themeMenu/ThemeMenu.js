2d970613c3825a1cfbd6e4c898b65fdde20f606b
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-01-26 02:08:56 +0700

    add theme menu icon

--- client/src/components/themeMenu/ThemeMenu.js
+++ client/src/components/themeMenu/ThemeMenu.js
@@ -1,8 +1,14 @@
-import { useRef } from 'react';
+// import { useRef } from 'react';
 import './themeMenu.css';
 
 const ThemeMenu = () => {
-    return <div>Theme Menu</div>;
+    return (
+        <div>
+            <button className='dropdown__toggle'>
+                <i className='bx bx-palette'></i>
+            </button>
+        </div>
+    );
 };
 
 export default ThemeMenu;
