0b81b0062b90531f6c308dd36c3de46439ea5996
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-01-26 02:07:54 +0700

    update topnav

--- client/src/components/topnav/TopNav.js
+++ client/src/components/topnav/TopNav.js
@@ -1,16 +1,72 @@
+import { Link } from 'react-router-dom';
 import './topNav.css';
+import Dropdown from '../dropdown/Dropdown';
+import ThemeMenu from '../themeMenu/ThemeMenu';
+import userAvatar from '../../assets/images/avatar.jpg';
+import userMenu from '../../configs/userMenu';
+import notifications from '../../configs/notifications';
+
+const userInfo = {
+    name: 'Tân',
+    image: userAvatar
+};
+
+const renderUserToggle = (user) => (
+    <div className='topnav__right-user'>
+        <div className='topnav__right-user__image'>
+            <img src={user.image} alt='avatar' />
+        </div>
+        <div className='topnav__right-user__name'>{user.name}</div>
+    </div>
+);
+
+const renderUserMenu = (item, index) => (
+    <Link to='/' key={index}>
+        <div className='notification-item'>
+            <i className={item.icon}></i>
+            <span>{item.content}</span>
+        </div>
+    </Link>
+);
+
+const renderNotificationItem = (item, index) => (
+    <div className='notification-item' key={index}>
+        <i className={item.icon}></i>
+        <span>{item.content}</span>
+    </div>
+);
 
 const TopNav = () => {
     return (
         <div className='topnav'>
             <div className='topnav__search'>
-                <input type='text' placeholder='Search here...' />
+                <input type='text' placeholder='Khum tìm được đâu' />
                 <i className='bx bx-search'></i>
             </div>
             <div className='topnav__right'>
-                <div className='topnav__right-item'>hihi</div>
-                <div className='topnav__right-item'>hihi</div>
-                <div className='topnav__right-item'>hihi</div>
+                <div className='topnav__right-item'>
+                    <Dropdown
+                        customToggle={() => renderUserToggle(userInfo)}
+                        contentData={userMenu}
+                        renderItems={(item, index) =>
+                            renderUserMenu(item, index)
+                        }
+                    />
+                </div>
+                <div className='topnav__right-item'>
+                    <Dropdown
+                        icon='bx bx-bell'
+                        badge='12'
+                        contentData={notifications}
+                        renderItems={(item, index) =>
+                            renderNotificationItem(item, index)
+                        }
+                        renderFooter={() => <Link to='/'>View All</Link>}
+                    />
+                </div>
+                <div className='topnav__right-item'>
+                    <ThemeMenu />
+                </div>
             </div>
         </div>
     );
