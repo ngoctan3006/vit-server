856fe6d22231c4fbd588e731852ab86eb5994808
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-01-27 20:55:20 +0700

    add not found page

--- client/src/pages/notFound/NotFound.js
+++ client/src/pages/notFound/NotFound.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import image from '../../assets/images/404.svg';
+import './notFound.css';
 
 const NotFound = () => {
-    return <div>Not Found</div>;
+    return (
+        <div className='not-found__container'>
+            <div className='not-found__inner'>
+                <img src={image} alt='image 404' />
+                <h1>{'Khum có gì ở đây đâu =)))'}</h1>
+                <Link to='/'>
+                    <div className='back-to-home'>Quay lại trang chủ</div>
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default NotFound;
