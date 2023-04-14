9605e7e89e7e5933f0c23496a889cf17a4ccd602
Tan <>
2023-04-14 20:38:44 +0700

    add remove accents, generate username

---
+++ src/shares/utils/remove-accents.util.ts
@@ -0,0 +1,25 @@
+export const removeAccents = (str: string): string => {
+  str = str.normalize('NFC');
+  const AccentsMap = [
+    'aàảãáạăằẳẵắặâầẩẫấậ',
+    'AÀẢÃÁẠĂẰẲẴẮẶÂẦẨẪẤẬ',
+    'dđ',
+    'DĐ',
+    'eèẻẽéẹêềểễếệ',
+    'EÈẺẼÉẸÊỀỂỄẾỆ',
+    'iìỉĩíị',
+    'IÌỈĨÍỊ',
+    'oòỏõóọôồổỗốộơờởỡớợ',
+    'OÒỎÕÓỌÔỒỔỖỐỘƠỜỞỠỚỢ',
+    'uùủũúụưừửữứự',
+    'UÙỦŨÚỤƯỪỬỮỨỰ',
+    'yỳỷỹýỵ',
+    'YỲỶỸÝỴ',
+  ];
+  for (let i = 0; i < AccentsMap.length; i++) {
+    const re = new RegExp(`[${AccentsMap[i].substring(1)}]`, 'g');
+    const char = AccentsMap[i][0];
+    str = str.replace(re, char);
+  }
+  return str;
+};
