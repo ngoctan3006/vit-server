78109371736ebefc2da96dca4fc6e784efdc3b67
Tan <>
2023-04-26 21:25:19 +0700

    setup department api

---
+++ src/modules/department/department.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { DepartmentService } from './department.service';
+import { CreateDepartmentDto } from './dto/create-department.dto';
+import { UpdateDepartmentDto } from './dto/update-department.dto';
+
+@Controller('department')
+@ApiTags('department')
+export class DepartmentController {
+  constructor(private readonly departmentService: DepartmentService) {}
+
+  @Post()
+  create(@Body() createDepartmentDto: CreateDepartmentDto) {
+    return this.departmentService.create(createDepartmentDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.departmentService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.departmentService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateDepartmentDto: UpdateDepartmentDto
+  ) {
+    return this.departmentService.update(+id, updateDepartmentDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.departmentService.remove(+id);
+  }
+}
