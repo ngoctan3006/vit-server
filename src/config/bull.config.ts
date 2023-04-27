eec0036034dfda888261ea7bdce77738247edae1
Tan <>
2023-04-27 14:13:47 +0700

    add bull config, cache config

---
+++ src/config/bull.config.ts
@@ -0,0 +1,15 @@
+import { SharedBullAsyncConfiguration } from '@nestjs/bull';
+import { ConfigService } from '@nestjs/config';
+import { EnvConstant } from './../shares/constants/env.constant';
+
+export const bullConfig: SharedBullAsyncConfiguration = {
+  useFactory: (configService: ConfigService) => ({
+    redis: {
+      host: configService.get<string>(EnvConstant.REDIS_HOST),
+      port: configService.get<number>(EnvConstant.REDIS_PORT),
+      username: configService.get<string>(EnvConstant.REDIS_USERNAME),
+      password: configService.get<string>(EnvConstant.REDIS_PASSWORD),
+    },
+  }),
+  inject: [ConfigService],
+};
