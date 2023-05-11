854604d2eac31334b1c8c82bb150f3f65fb390f3
Tan <>
2023-05-11 10:53:56 +0700

    add message

---
+++ src/shares/message/index.ts
@@ -0,0 +1,69 @@
+export const messageSuccess = {
+  // activity
+  ACTIVITY_REGISTER: {
+    message: 'Bạn đã đăng ký hoạt động thành công',
+    code: 'ACTIVITY_1001',
+  },
+  ACTIVITY_CANCEL: {
+    message: 'Bạn đã xin nghỉ hoạt động thành công',
+    code: 'ACTIVITY_1002',
+  },
+  ACTIVITY_DELETE: {
+    message: 'Xoá hoạt động thành công',
+    code: 'ACTIVITY_1003',
+  },
+  ACTIVITY_APPROVE: {
+    message: 'Đã chấp nhận tài khoản người dùng thành công',
+    code: 'ACTIVITY_1004',
+  },
+
+  // user
+  USER_REQUEST_RESET_PASSWORD: {
+    message: 'Đường link đặt lại mật khẩu đã được gửi vào email của bạn',
+    code: 'USER_1001',
+  },
+  USER_CHANGE_PASSWORD: {
+    message: 'Thay đổi mật khẩu thành công',
+    code: 'USER_1002',
+  },
+  USER_RESET_PASSWORD: {
+    message: 'Đặt lại mật khẩu thành công',
+    code: 'USER_1003',
+  },
+
+  // club
+  CLUB_DELETE: {
+    message: 'Xoá câu lạc bộ thành công',
+    code: 'CLUB_1001',
+  },
+
+  // department
+  DEPARTMENT_DELETE: {
+    message: 'Xoá mảng thành công',
+    code: 'DEPARTMENT_1001',
+  },
+
+  // event
+  EVENT_REGISTER: {
+    message: 'Bạn đã đăng ký sự kiện thành công',
+    code: 'EVENT_1001',
+  },
+  EVENT_CANCEL: {
+    message: 'Bạn đã xin nghỉ sự kiện thành công',
+    code: 'EVENT_1002',
+  },
+  EVENT_DELETE: {
+    message: 'Xoá sự kiện thành công',
+    code: 'EVENT_1003',
+  },
+  EVENT_APPROVE: {
+    message: 'Đã chấp nhận tài khoản người dùng thành công',
+    code: 'EVENT_1004',
+  },
+
+  // group
+  GROUP_DELETE: {
+    message: 'Xoá nhóm thành công',
+    code: 'GROUP_1001',
+  },
+};
