ba4336ccc9264f10298aa752a692f5715e21a9d1
Tan <ngoctana1k2014@gmail.com>
2023-07-24 11:31:13 +0700

    update min value of pagination

--- src/shares/dto/pagination.dto.ts
+++ src/shares/dto/pagination.dto.ts
@@ -1,15 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
-import { IsOptional } from 'class-validator';
+import { IsOptional, Min } from 'class-validator';
 
 export class PaginationDto {
   @ApiProperty({ required: false })
   @Transform(({ value }) => parseInt(value))
   @IsOptional()
+  @Min(1)
   page?: number = 1;
 
   @ApiProperty({ required: false })
   @Transform(({ value }) => parseInt(value))
   @IsOptional()
+  @Min(1)
   limit?: number = 10;
 }
