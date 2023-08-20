ee0d3fdb23100b1e8ced931e6094bf5d382d0421
Tan <ngoctana1k2014@gmail.com>
2023-08-21 01:42:26 +0700

    add club entity

---
+++ src/modules/club/entities/user-club.entity.ts
@@ -0,0 +1,17 @@
+import { Position, UserStatus } from 'src/modules/user/enums';
+import { BaseEntity } from 'src/shares/entities';
+import { Column, ObjectId } from 'typeorm';
+
+export class UserClubEntity extends BaseEntity {
+  @Column()
+  clubId: ObjectId;
+
+  @Column()
+  userId: ObjectId;
+
+  @Column({ type: 'enum', enum: Position, default: Position.MEMBER })
+  position: Position;
+
+  @Column({ type: 'enum', enum: UserStatus, default: UserStatus.ACTIVE })
+  status: UserStatus;
+}
