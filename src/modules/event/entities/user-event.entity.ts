bf67cbbb1dc4d14667ab4255912b6291210a1d8e
Tan <ngoctana1k2014@gmail.com>
2023-08-22 15:14:43 +0700

    update user event entity

---
+++ src/modules/event/entities/user-event.entity.ts
@@ -0,0 +1,23 @@
+import { Position } from 'src/modules/user/enums';
+import { BaseEntity } from 'src/shares/entities';
+import { UserJoinStatus } from 'src/shares/enums';
+import { Column, Entity, ObjectId } from 'typeorm';
+
+@Entity('user_event')
+export class UserEventEntity extends BaseEntity {
+  @Column()
+  userId: ObjectId;
+
+  @Column()
+  eventId: ObjectId;
+
+  @Column({ type: 'enum', enum: Position, default: Position.THANH_VIEN })
+  position: Position;
+
+  @Column({
+    type: 'enum',
+    enum: UserJoinStatus,
+    default: UserJoinStatus.REGISTERED,
+  })
+  status: UserJoinStatus;
+}
