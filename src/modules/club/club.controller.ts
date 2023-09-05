b71782f5eeedd812ad6d19316db6f985c15c7291
Tan <ngoctana1k2014@gmail.com>
2023-09-05 16:10:33 +0700

    refactor club module

--- src/modules/club/club.controller.ts
+++ src/modules/club/club.controller.ts
@@ -1,6 +1,21 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Club, Position } from '@prisma/client';
+import { Roles } from 'src/shares/decorators';
+import { MessageDto, PaginationDto, ResponseDto } from 'src/shares/dto';
+import { JwtGuard } from '../auth/guards';
 import { ClubService } from './club.service';
+import { CreateClubDto, UpdateClubDto } from './dto';
 
 @Controller('club')
 @ApiTags('club')
@@ -8,55 +23,55 @@ import { ClubService } from './club.service';
 export class ClubController {
   constructor(private readonly clubService: ClubService) {}
 
-  // @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
-  // @Post()
-  // async create(@Body() data: CreateClubDto): Promise<ResponseDto<Club>> {
-  //   return await this.clubService.create(data);
-  // }
+  @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
+  @Post()
+  async create(@Body() data: CreateClubDto): Promise<ResponseDto<Club>> {
+    return await this.clubService.create(data);
+  }
 
-  // @UseGuards(JwtGuard)
-  // @Get()
-  // async findAll(
-  //   @Query() { page, limit }: PaginationDto
-  // ): Promise<ResponseDto<Club[]>> {
-  //   return await this.clubService.findAll(page, limit);
-  // }
+  @UseGuards(JwtGuard)
+  @Get()
+  async findAll(
+    @Query() { page, limit }: PaginationDto
+  ): Promise<ResponseDto<Club[]>> {
+    return await this.clubService.findAll(page, limit);
+  }
 
-  // @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
-  // @Get('trash')
-  // async findAllDeleted(
-  //   @Query() { page, limit }: PaginationDto
-  // ): Promise<ResponseDto<Club[]>> {
-  //   return await this.clubService.findAllDeleted(page, limit);
-  // }
+  @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
+  @Get('trash')
+  async findAllDeleted(
+    @Query() { page, limit }: PaginationDto
+  ): Promise<ResponseDto<Club[]>> {
+    return await this.clubService.findAllDeleted(page, limit);
+  }
 
-  // @UseGuards(JwtGuard)
-  // @Get(':id')
-  // async findOne(@Param('id') id: number): Promise<ResponseDto<Club>> {
-  //   return await this.clubService.findOne(id);
-  // }
+  @UseGuards(JwtGuard)
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<ResponseDto<Club>> {
+    return await this.clubService.findOne(id);
+  }
 
-  // @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
-  // @Get('trash/:id')
-  // async findOneDeleted(@Param('id') id: number): Promise<ResponseDto<Club>> {
-  //   return await this.clubService.findOneDeleted(id);
-  // }
+  @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
+  @Get('trash/:id')
+  async findOneDeleted(@Param('id') id: string): Promise<ResponseDto<Club>> {
+    return await this.clubService.findOneDeleted(id);
+  }
 
-  // @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
-  // @Put(':id')
-  // async update(@Param('id') id: number, @Body() data: UpdateClubDto) {
-  //   return await this.clubService.update(id, data);
-  // }
+  @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() data: UpdateClubDto) {
+    return await this.clubService.update(id, data);
+  }
 
-  // @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
-  // @Delete(':id')
-  // async softDelete(@Param('id') id: number): Promise<ResponseDto<MessageDto>> {
-  //   return await this.clubService.softRemove(id);
-  // }
+  @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
+  @Delete(':id')
+  async softDelete(@Param('id') id: string): Promise<ResponseDto<MessageDto>> {
+    return await this.clubService.softRemove(id);
+  }
 
-  // @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
-  // @Put('restore/:id')
-  // async restore(@Param('id') id: number): Promise<ResponseDto<Club>> {
-  //   return await this.clubService.restore(id);
-  // }
+  @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
+  @Put('restore/:id')
+  async restore(@Param('id') id: string): Promise<ResponseDto<Club>> {
+    return await this.clubService.restore(id);
+  }
 }
--- src/modules/club/club.service.ts
+++ src/modules/club/club.service.ts
@@ -1,106 +1,113 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Club } from '@prisma/client';
+import { MessageDto, ResponseDto } from 'src/shares/dto';
+import { httpErrors } from 'src/shares/exception';
+import { messageSuccess } from 'src/shares/message';
 import { DepartmentService } from './../department/department.service';
+import { PrismaService } from './../prisma/prisma.service';
+import { CreateClubDto, UpdateClubDto } from './dto';
 
 @Injectable()
 export class ClubService {
-  constructor(private readonly departmentService: DepartmentService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly departmentService: DepartmentService
+  ) {}
 
-  // async create(data: CreateClubDto): Promise<ResponseDto<Club>> {
-  //   await this.departmentService.findOne(data.department_id);
-  //   return { data: await this.prisma.club.create({ data }) };
-  // }
+  async create(data: CreateClubDto): Promise<ResponseDto<Club>> {
+    await this.departmentService.findOne(data.departmentId);
+    return { data: await this.prisma.club.create({ data }) };
+  }
 
-  // async findAll(page: number, limit: number): Promise<ResponseDto<Club[]>> {
-  //   if (isNaN(page) || isNaN(limit))
-  //     throw new HttpException(httpErrors.QUERY_INVALID, HttpStatus.BAD_REQUEST);
+  async findAll(page: number, limit: number): Promise<ResponseDto<Club[]>> {
+    if (isNaN(page) || isNaN(limit))
+      throw new HttpException(httpErrors.QUERY_INVALID, HttpStatus.BAD_REQUEST);
 
-  //   return {
-  //     data: await this.prisma.club.findMany({
-  //       where: { deleted_at: null },
-  //       skip: (page - 1) * limit,
-  //       take: limit,
-  //       orderBy: {
-  //         created_at: 'desc',
-  //       },
-  //     }),
-  //     metadata: {
-  //       totalPage: Math.ceil(
-  //         (await this.prisma.club.count({
-  //           where: { deleted_at: null },
-  //         })) / limit
-  //       ),
-  //     },
-  //   };
-  // }
+    return {
+      data: await this.prisma.club.findMany({
+        where: { deletedAt: null },
+        skip: (page - 1) * limit,
+        take: limit,
+        orderBy: { createdAt: 'desc' },
+      }),
+      pagination: {
+        totalPage: Math.ceil(
+          (await this.prisma.club.count({
+            where: { deletedAt: null },
+          })) / limit
+        ),
+      },
+    };
+  }
 
-  // async findAllDeleted(
-  //   page: number,
-  //   limit: number
-  // ): Promise<ResponseDto<Club[]>> {
-  //   if (isNaN(page) || isNaN(limit))
-  //     throw new HttpException(httpErrors.QUERY_INVALID, HttpStatus.BAD_REQUEST);
+  async findAllDeleted(
+    page: number,
+    limit: number
+  ): Promise<ResponseDto<Club[]>> {
+    if (isNaN(page) || isNaN(limit))
+      throw new HttpException(httpErrors.QUERY_INVALID, HttpStatus.BAD_REQUEST);
 
-  //   return {
-  //     data: await this.prisma.club.findMany({
-  //       where: { deleted_at: { not: null } },
-  //       skip: (page - 1) * limit,
-  //       take: limit,
-  //       orderBy: {
-  //         deleted_at: 'desc',
-  //       },
-  //     }),
-  //     metadata: {
-  //       totalPage: Math.ceil(
-  //         (await this.prisma.club.count({
-  //           where: { deleted_at: { not: null } },
-  //         })) / limit
-  //       ),
-  //     },
-  //   };
-  // }
+    return {
+      data: await this.prisma.club.findMany({
+        where: { deletedAt: { not: null } },
+        skip: (page - 1) * limit,
+        take: limit,
+        orderBy: {
+          deletedAt: 'desc',
+        },
+      }),
+      pagination: {
+        totalPage: Math.ceil(
+          (await this.prisma.club.count({
+            where: { deletedAt: { not: null } },
+          })) / limit
+        ),
+      },
+    };
+  }
 
-  // async findOne(id: number): Promise<ResponseDto<Club>> {
-  //   const club = await this.prisma.club.findUnique({
-  //     where: { id },
-  //   });
-  //   if (!club || club.deleted_at) {
-  //     throw new HttpException(httpErrors.CLUB_NOT_FOUND, HttpStatus.NOT_FOUND);
-  //   }
-  //   return { data: club };
-  // }
+  async findOne(id: string): Promise<ResponseDto<Club>> {
+    const club = await this.prisma.club.findUnique({
+      where: { id },
+    });
+    if (!club || club.deletedAt) {
+      throw new HttpException(httpErrors.CLUB_NOT_FOUND, HttpStatus.NOT_FOUND);
+    }
+    return { data: club };
+  }
 
-  // async findOneDeleted(id: number): Promise<ResponseDto<Club>> {
-  //   const club = await this.prisma.club.findUnique({
-  //     where: { id },
-  //   });
-  //   if (!club || !club.deleted_at) {
-  //     throw new HttpException(httpErrors.CLUB_NOT_FOUND, HttpStatus.NOT_FOUND);
-  //   }
-  //   return { data: club };
-  // }
+  async findOneDeleted(id: string): Promise<ResponseDto<Club>> {
+    const club = await this.prisma.club.findUnique({
+      where: { id },
+    });
+    if (!club || !club.deletedAt) {
+      throw new HttpException(httpErrors.CLUB_NOT_FOUND, HttpStatus.NOT_FOUND);
+    }
+    return { data: club };
+  }
 
-  // async update(id: number, data: UpdateClubDto): Promise<ResponseDto<Club>> {
-  //   await this.findOne(id);
-  //   if (data.department_id)
-  //     await this.departmentService.findOne(data.department_id);
-  //   return { data: await this.prisma.club.update({ where: { id }, data }) };
-  // }
+  async update(id: string, data: UpdateClubDto): Promise<ResponseDto<Club>> {
+    await this.findOne(id);
+    if (data.departmentId)
+      await this.departmentService.findOne(data.departmentId);
+    return { data: await this.prisma.club.update({ where: { id }, data }) };
+  }
 
-  // async softRemove(id: number): Promise<ResponseDto<MessageDto>> {
-  //   await this.findOne(id);
-  //   await this.prisma.club.update({
-  //     where: { id },
-  //     data: { deleted_at: new Date() },
-  //   });
-  //   return { data: messageSuccess.CLUB_DELETE };
-  // }
+  async softRemove(id: string): Promise<ResponseDto<MessageDto>> {
+    await this.findOne(id);
+    await this.prisma.club.update({
+      where: { id },
+      data: { deletedAt: new Date() },
+    });
+    return { data: messageSuccess.CLUB_DELETE };
+  }
 
-  // async restore(id: number): Promise<ResponseDto<Club>> {
-  //   await this.findOneDeleted(id);
-  //   await this.prisma.club.update({
-  //     where: { id },
-  //     data: { deleted_at: null },
-  //   });
-  //   return await this.findOne(id);
-  // }
+  async restore(id: string): Promise<ResponseDto<Club>> {
+    await this.findOneDeleted(id);
+    await this.prisma.club.update({
+      where: { id },
+      data: { deletedAt: null },
+    });
+    return await this.findOne(id);
+  }
 }
