f438da306bbf5791e1b5880e8300b9fa828510c7
Tan <ngoctana1k2014@gmail.com>
2023-09-05 16:15:15 +0700

    update create event dto

--- src/modules/event/dto/create-event.dto.ts
+++ src/modules/event/dto/create-event.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateEventDto {
@@ -14,13 +15,18 @@ export class CreateEventDto {
 
   @ApiProperty()
   @IsNotEmpty()
-  @IsString()
-  startTime: string;
+  @Transform(({ value }) => new Date(value))
+  deadline: Date;
 
   @ApiProperty()
   @IsNotEmpty()
-  @IsString()
-  endTime: string;
+  @Transform(({ value }) => new Date(value))
+  startTime: Date;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @Transform(({ value }) => new Date(value))
+  endTime: Date;
 
   @ApiProperty()
   @IsNotEmpty()
