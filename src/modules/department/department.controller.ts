e40aaa415b03f0e6fc3d0a52531d9febc8c41168
Tan <ngoctana1k2014@gmail.com>
2023-09-05 16:12:22 +0700

    refactor department module

--- src/modules/department/department.controller.ts
+++ src/modules/department/department.controller.ts
@@ -1,6 +1,21 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Department, Position } from '@prisma/client';
+import { Roles } from 'src/shares/decorators';
+import { MessageDto, PaginationDto, ResponseDto } from 'src/shares/dto';
+import { JwtGuard } from '../auth/guards';
 import { DepartmentService } from './department.service';
+import { CreateDepartmentDto, UpdateDepartmentDto } from './dto';
 
 @Controller('department')
 @ApiTags('department')
@@ -8,59 +23,59 @@ import { DepartmentService } from './department.service';
 export class DepartmentController {
   constructor(private readonly departmentService: DepartmentService) {}
 
-  // @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
-  // @Post()
-  // async create(
-  //   @Body() data: CreateDepartmentDto
-  // ): Promise<ResponseDto<Department>> {
-  //   return await this.departmentService.create(data);
-  // }
+  @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
+  @Post()
+  async create(
+    @Body() data: CreateDepartmentDto
+  ): Promise<ResponseDto<Department>> {
+    return await this.departmentService.create(data);
+  }
 
-  // @UseGuards(JwtGuard)
-  // @Get()
-  // async findAll(
-  //   @Query() { page, limit }: PaginationDto
-  // ): Promise<ResponseDto<Department[]>> {
-  //   return await this.departmentService.findAll(page, limit);
-  // }
+  @UseGuards(JwtGuard)
+  @Get()
+  async findAll(
+    @Query() { page, limit }: PaginationDto
+  ): Promise<ResponseDto<Department[]>> {
+    return await this.departmentService.findAll(page, limit);
+  }
 
-  // @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
-  // @Get('trash')
-  // async findAllDeleted(
-  //   @Query() { page, limit }: PaginationDto
-  // ): Promise<ResponseDto<Department[]>> {
-  //   return await this.departmentService.findAllDeleted(page, limit);
-  // }
+  @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
+  @Get('trash')
+  async findAllDeleted(
+    @Query() { page, limit }: PaginationDto
+  ): Promise<ResponseDto<Department[]>> {
+    return await this.departmentService.findAllDeleted(page, limit);
+  }
 
-  // @UseGuards(JwtGuard)
-  // @Get(':id')
-  // async findOne(@Param('id') id: number): Promise<ResponseDto<Department>> {
-  //   return await this.departmentService.findOne(id);
-  // }
+  @UseGuards(JwtGuard)
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<ResponseDto<Department>> {
+    return await this.departmentService.findOne(id);
+  }
 
-  // @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
-  // @Get('trash/:id')
-  // async findOneDeleted(
-  //   @Param('id') id: number
-  // ): Promise<ResponseDto<Department>> {
-  //   return await this.departmentService.findOneDeleted(id);
-  // }
+  @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
+  @Get('trash/:id')
+  async findOneDeleted(
+    @Param('id') id: string
+  ): Promise<ResponseDto<Department>> {
+    return await this.departmentService.findOneDeleted(id);
+  }
 
-  // @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
-  // @Put(':id')
-  // async update(@Param('id') id: number, @Body() data: UpdateDepartmentDto) {
-  //   return await this.departmentService.update(id, data);
-  // }
+  @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() data: UpdateDepartmentDto) {
+    return await this.departmentService.update(id, data);
+  }
 
-  // @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
-  // @Delete(':id')
-  // async softDelete(@Param('id') id: number): Promise<ResponseDto<MessageDto>> {
-  //   return await this.departmentService.softRemove(id);
-  // }
+  @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
+  @Delete(':id')
+  async softDelete(@Param('id') id: string): Promise<ResponseDto<MessageDto>> {
+    return await this.departmentService.softRemove(id);
+  }
 
-  // @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
-  // @Put('restore/:id')
-  // async restore(@Param('id') id: number): Promise<ResponseDto<Department>> {
-  //   return await this.departmentService.restore(id);
-  // }
+  @Roles(Position.ADMIN, Position.DOI_TRUONG, Position.DOI_PHO)
+  @Put('restore/:id')
+  async restore(@Param('id') id: string): Promise<ResponseDto<Department>> {
+    return await this.departmentService.restore(id);
+  }
 }
--- src/modules/department/department.service.ts
+++ src/modules/department/department.service.ts
@@ -1,112 +1,118 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Department } from '@prisma/client';
+import { MessageDto, ResponseDto } from 'src/shares/dto';
+import { httpErrors } from 'src/shares/exception';
+import { messageSuccess } from 'src/shares/message';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateDepartmentDto, UpdateDepartmentDto } from './dto';
 
 @Injectable()
 export class DepartmentService {
-  constructor() {}
+  constructor(private readonly prisma: PrismaService) {}
 
-  // async create(data: CreateDepartmentDto): Promise<ResponseDto<Department>> {
-  //   return { data: await this.prisma.department.create({ data }) };
-  // }
+  async create(data: CreateDepartmentDto): Promise<ResponseDto<Department>> {
+    return { data: await this.prisma.department.create({ data }) };
+  }
 
-  // async findAll(
-  //   page: number,
-  //   limit: number
-  // ): Promise<ResponseDto<Department[]>> {
-  //   if (isNaN(page) || isNaN(limit))
-  //     throw new HttpException(httpErrors.QUERY_INVALID, HttpStatus.BAD_REQUEST);
+  async findAll(
+    page: number,
+    limit: number
+  ): Promise<ResponseDto<Department[]>> {
+    if (isNaN(page) || isNaN(limit))
+      throw new HttpException(httpErrors.QUERY_INVALID, HttpStatus.BAD_REQUEST);
 
-  //   return {
-  //     data: await this.prisma.department.findMany({
-  //       where: { deleted_at: null },
-  //       skip: (page - 1) * limit,
-  //       take: limit,
-  //       orderBy: {
-  //         created_at: 'desc',
-  //       },
-  //     }),
-  //     metadata: {
-  //       totalPage: Math.ceil(
-  //         (await this.prisma.department.count({
-  //           where: { deleted_at: null },
-  //         })) / limit
-  //       ),
-  //     },
-  //   };
-  // }
+    return {
+      data: await this.prisma.department.findMany({
+        where: { deletedAt: null },
+        skip: (page - 1) * limit,
+        take: limit,
+        orderBy: {
+          createdAt: 'desc',
+        },
+      }),
+      pagination: {
+        totalPage: Math.ceil(
+          (await this.prisma.department.count({
+            where: { deletedAt: null },
+          })) / limit
+        ),
+      },
+    };
+  }
 
-  // async findAllDeleted(
-  //   page: number,
-  //   limit: number
-  // ): Promise<ResponseDto<Department[]>> {
-  //   if (isNaN(page) || isNaN(limit))
-  //     throw new HttpException(httpErrors.QUERY_INVALID, HttpStatus.BAD_REQUEST);
+  async findAllDeleted(
+    page: number,
+    limit: number
+  ): Promise<ResponseDto<Department[]>> {
+    if (isNaN(page) || isNaN(limit))
+      throw new HttpException(httpErrors.QUERY_INVALID, HttpStatus.BAD_REQUEST);
 
-  //   return {
-  //     data: await this.prisma.department.findMany({
-  //       where: { deleted_at: { not: null } },
-  //       skip: (page - 1) * limit,
-  //       take: limit,
-  //       orderBy: {
-  //         deleted_at: 'desc',
-  //       },
-  //     }),
-  //     metadata: {
-  //       totalPage: Math.ceil(
-  //         (await this.prisma.department.count({
-  //           where: { deleted_at: { not: null } },
-  //         })) / limit
-  //       ),
-  //     },
-  //   };
-  // }
+    return {
+      data: await this.prisma.department.findMany({
+        where: { deletedAt: { not: null } },
+        skip: (page - 1) * limit,
+        take: limit,
+        orderBy: {
+          deletedAt: 'desc',
+        },
+      }),
+      pagination: {
+        totalPage: Math.ceil(
+          (await this.prisma.department.count({
+            where: { deletedAt: { not: null } },
+          })) / limit
+        ),
+      },
+    };
+  }
 
-  // async findOne(id: number): Promise<ResponseDto<Department>> {
-  //   const department = await this.prisma.department.findUnique({
-  //     where: { id },
-  //   });
-  //   if (!department || department.deleted_at) {
-  //     throw new HttpException(
-  //       httpErrors.DEPARTMENT_NOT_FOUND,
-  //       HttpStatus.NOT_FOUND
-  //     );
-  //   }
-  //   return { data: department };
-  // }
+  async findOne(id: string): Promise<ResponseDto<Department>> {
+    const department = await this.prisma.department.findUnique({
+      where: { id },
+    });
+    if (!department || department.deletedAt) {
+      throw new HttpException(
+        httpErrors.DEPARTMENT_NOT_FOUND,
+        HttpStatus.NOT_FOUND
+      );
+    }
+    return { data: department };
+  }
 
-  // async findOneDeleted(id: number): Promise<ResponseDto<Department>> {
-  //   const department = await this.prisma.department.findUnique({
-  //     where: { id },
-  //   });
-  //   if (!department || !department.deleted_at) {
-  //     throw new HttpException(
-  //       httpErrors.DEPARTMENT_NOT_FOUND,
-  //       HttpStatus.NOT_FOUND
-  //     );
-  //   }
-  //   return { data: department };
-  // }
+  async findOneDeleted(id: string): Promise<ResponseDto<Department>> {
+    const department = await this.prisma.department.findUnique({
+      where: { id },
+    });
+    if (!department || !department.deletedAt) {
+      throw new HttpException(
+        httpErrors.DEPARTMENT_NOT_FOUND,
+        HttpStatus.NOT_FOUND
+      );
+    }
+    return { data: department };
+  }
 
-  // async update(id: number, data: UpdateDepartmentDto): Promise<Department> {
-  //   await this.findOne(id);
-  //   return await this.prisma.department.update({ where: { id }, data });
-  // }
+  async update(id: string, data: UpdateDepartmentDto): Promise<Department> {
+    await this.findOne(id);
+    return await this.prisma.department.update({ where: { id }, data });
+  }
 
-  // async softRemove(id: number): Promise<ResponseDto<MessageDto>> {
-  //   await this.findOne(id);
-  //   await this.prisma.department.update({
-  //     where: { id },
-  //     data: { deleted_at: new Date() },
-  //   });
-  //   return { data: messageSuccess.DEPARTMENT_DELETE };
-  // }
+  async softRemove(id: string): Promise<ResponseDto<MessageDto>> {
+    await this.findOne(id);
+    await this.prisma.department.update({
+      where: { id },
+      data: { deletedAt: new Date() },
+    });
+    return { data: messageSuccess.DEPARTMENT_DELETE };
+  }
 
-  // async restore(id: number): Promise<ResponseDto<Department>> {
-  //   await this.findOneDeleted(id);
-  //   await this.prisma.department.update({
-  //     where: { id },
-  //     data: { deleted_at: null },
-  //   });
+  async restore(id: string): Promise<ResponseDto<Department>> {
+    await this.findOneDeleted(id);
+    await this.prisma.department.update({
+      where: { id },
+      data: { deletedAt: null },
+    });
 
-  //   return await this.findOne(id);
-  // }
+    return await this.findOne(id);
+  }
 }
